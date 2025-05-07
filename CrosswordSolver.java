36d4d000cf33d327a7e59dfc0126063e024368e6
Squiddy24 <130530087+Squiddy24@users.noreply.github.com>
2025-05-07 11:15:01 -0400

    all comments and some minor optimization

--- CrosswordSolver.java
+++ CrosswordSolver.java
@@ -12,7 +12,6 @@ import java.awt.Color;
 
 public class CrosswordSolver extends JFrame {
     public static void main(String[] args) {
-        //TODO let them run again
         //A Hashmap that converts a number (0-7) to a direction matrix
         //0 is north and it rotates clockwise
         HashMap <Integer,int[]> directionMap = new HashMap<Integer,int[]>();
@@ -27,53 +26,42 @@ public class CrosswordSolver extends JFrame {
 
         //Input used for reading the terminal
         Scanner input = new Scanner(System.in);
+        boolean runFile = true;
 
-        //String array with a list of words
-        String[] wordList = makeWordList(input);
+        while (runFile) {
+            //String array with a list of words
+            String[] wordList = makeWordList(input);
 
-        //Creates the crossword grid
-        char[][] crosswordGrid = readCrossword(getInputFile(input));
+            //Creates the crossword grid
+            char[][] crosswordGrid = readCrossword(getInputFile(input));
 
-        //Creates an array to store the solution
-        char[][] solutionGrid = new char[crosswordGrid.length][crosswordGrid[0].length];
+            // Start of the execution timer
+            double startTime = System.nanoTime(); 
+            
+            //Generates a solved wordsearch
+            char[][] solutionGrid = solveCrossword(crosswordGrid, wordList, directionMap);
 
-        // Start of the execution timer
-        double startTime = System.nanoTime(); 
-        
-        for (String word : wordList) { //for each word
-            wordLoop:
-            for (int i = 0; i < crosswordGrid.length; i++) { //for each column
-                for (int j = 0; j < crosswordGrid[0].length; j++) { //for each row
-                    if (crosswordGrid[i][j] == word.charAt(0)) { // if the character at a given position match the first letter of the word
-                        for (int angle = 0; angle < 8; angle++) { // for each angle
-                            try {
-                                for (int letter = 0; letter < word.length(); letter++){ //Moves forward and checks if the it matches the word
-                                    //If it does not match throw an error
-                                    if (crosswordGrid[i + (directionMap.get(angle)[1] * letter)][j + (directionMap.get(angle)[0] * letter)] != word.charAt(letter)){
-                                        throw new Exception();
-                                    }
-                                }
+            // End of the execution timer
+            double stopTime = System.nanoTime();
 
-                                //If the maches it is found and writen to the solution grid
-                                for (int letter = 0; letter < word.length(); letter++){
-                                    solutionGrid[i + (directionMap.get(angle)[1] * letter)][j + (directionMap.get(angle)[0] * letter)] = word.charAt(letter);
-                                }
-                                //Moves on to the next word
-                                break wordLoop;
-                            } catch (Exception e) {
-                                continue;
-                            }
-                        }
-                    }
+            //Displays the window
+            new CrosswordSolver(solutionGrid, crosswordGrid,(stopTime-startTime) / 1000000);
+
+            //Displays a message to the user to show that the program is compleat
+            System.out.println("Wordsearch Solved!");
+
+            //Promps the user to run again
+            while (true) {
+                System.out.print("\nRun Program Again (y/n): ");
+                String response = input.nextLine();
+                if (response.matches("n")){
+                    runFile = false;
+                    break;
                 }
+                else if (response.matches("y")){break;}
+                else{System.out.println("Invalid Input Must Be 'y' or 'n'");}
             }
         }
-
-        // End of the execution timer
-        double stopTime = System.nanoTime();
-
-        //Displays the window
-        new CrosswordSolver(solutionGrid, crosswordGrid,(stopTime-startTime) / 1000000);
     }
 
     //The visuals output of the file
@@ -159,40 +147,79 @@ public class CrosswordSolver extends JFrame {
         }
     }
     
+    /*
+     * Solve Crossword
+     * Finds all words in the crossword
+     * @param wordsearchgrid the word search grid to be searched
+     * @param wordList the list of words to find
+     * @param directionMap a refrance to all posible directions a word could be placed in
+     * @returns char[][] a 2d array will all found words in it and letters not part of the words repersented as null
+     */
+    public static char[][] solveCrossword(char[][] wordsearchGrid, String[] wordList, HashMap<Integer,int[]> directionMap) {
+        
+        //Creates an array to store the solution
+        char[][] solutionGrid = new char[wordsearchGrid.length][wordsearchGrid[0].length];
+
+
+        for (String word : wordList) { //for each word
+            wordLoop:
+            for (int i = 0; i < wordsearchGrid.length; i++) { //for each row
+                for (int j = 0; j < wordsearchGrid[0].length; j++) { //for each column
+                    if (wordsearchGrid[i][j] == word.charAt(0)) { // if the character at a given position match the first letter of the word
+                        for (int angle = 0; angle < 8; angle++) { // for each angle
+                            try {
+                                for (int letter = 0; letter < word.length(); letter++){ //Moves forward and checks if the it matches the word
+                                    //If it does not match throw an error
+                                    if (wordsearchGrid[i + (directionMap.get(angle)[1] * letter)][j + (directionMap.get(angle)[0] * letter)] != word.charAt(letter)){
+                                        throw new Exception();
+                                    }
+                                }
+
+                                //If the maches it is found and writen to the solution grid
+                                for (int letter = 0; letter < word.length(); letter++){
+                                    solutionGrid[i + (directionMap.get(angle)[1] * letter)][j + (directionMap.get(angle)[0] * letter)] = word.charAt(letter);
+                                }
+                                //Moves on to the next word
+                                break wordLoop;
+                            } catch (Exception e) {
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return solutionGrid;
+    }
+
+    /*
+     * Read Crossword
+     * Converts the file into a 2d char array
+     * @param crosswordFile a scanner scanning the crossword file
+     * @returns char[][] a 2d array of the wordsearch
+     */
     public static char[][] readCrossword(Scanner crosswordFile){
-        //TODO fix this its so bad :(
-        int row = 0;
-        String line = crosswordFile.nextLine();
-        int column = line.length();
-        char[][] crosswordGrid = new char[20][column];
-
-        for (int letter = 0; letter < line.length(); letter++) {
-            crosswordGrid[row][letter] = line.charAt(letter);
-        }
-        row+=1;
+        //Creates a wordsearch of varible length
+        ArrayList<char[]> wordsearch = new ArrayList<char[]>();
+        
+        //Fills the array with all characters
         while (crosswordFile.hasNext()) {
-            line = crosswordFile.nextLine();
-            for (int letter = 0; letter < line.length(); letter++) {
-                crosswordGrid[row][letter] = line.charAt(letter);
-
-            }
-            row++;
+            wordsearch.add(crosswordFile.nextLine().toCharArray());
         }
 
-        int rows;
-        for (rows = 0; rows < crosswordGrid.length; rows++) {
-            if (crosswordGrid[rows][0] == '\u0000') {
-                break;
-            }
-        }
-        char[][] returnGrid = new char[rows][column];
-        for (int i = 0; i < returnGrid.length; i++) {
-            for (int j = 0; j < returnGrid[0].length; j++) {
-                returnGrid[i][j] = crosswordGrid[i][j];
+        //Creates a fixed size array
+        char[][] returnWordsearch = new char[wordsearch.size()][wordsearch.get(0).length];
+
+        //Fills the fixed size array with data from the varible length one
+        for (int row = 0; row < wordsearch.size(); row++) {
+            for (int column = 0; column < wordsearch.get(0).length; column++) {
+                returnWordsearch[row][column] = wordsearch.get(row)[column];
             }
         }
 
-        return returnGrid;
+        //Returns the updated word search
+        return returnWordsearch;
     }
 }
 
@@ -201,17 +228,27 @@ class Cell {
     //The contents of each square of the grid
     JLabel element;
 
+    //Constructor class for the cell class
     public Cell(int row,int column, char[][] wordSearch, char[][] solutionGrid) {
+        //Creates a Jlabel and sets its text to the corosponding grid
         this.element = new JLabel(Character.toString(wordSearch[row][column]).toUpperCase(), SwingConstants.CENTER);
+
+        //If the cell is not part of the solution
         if (solutionGrid[row][column] != '\u0000') {
+            //Set text to dark green and background to light green
             element.setBackground(new Color(205,231,202));
             element.setForeground(new Color(114,131,112));
-            element.setText("<html><b>"+Character.toString(wordSearch[row][column]).toUpperCase()+"</b></html>"); //Bold
+            
+            //Bolds the text
+            element.setText("<html><b>"+Character.toString(wordSearch[row][column]).toUpperCase()+"</b></html>");
         } 
         else {
+            //Set text to light green and background to dark green
             element.setBackground(new Color(114,131,112));
             element.setForeground(new Color(205,231,202));
         }
+
+        //Adds a border
         element.setBorder(BorderFactory.createLineBorder(new Color(75,81,74),2));
         element.setOpaque(true);
 
